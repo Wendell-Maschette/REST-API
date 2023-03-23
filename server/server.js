7fe5989711a92232d8620ba072cac950eb2db977
Wendell Maschette <wendell.maschette@gmail.com>
2023-03-23 20:27:32 -0300

    Desenvolvimento de CRUD com TDD

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use('/', require('./route/postsRoute'));
+console.log('Server is listening on port 3000');
 
-app.listen(3000, () =>
-{
-    app.use('/', require('./route/postsRoute'));
-    console.log('Server is listening on port 3000');
-});
+app.listen(3000);
